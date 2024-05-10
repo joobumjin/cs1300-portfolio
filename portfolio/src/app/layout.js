cd870e124f5bc00ce30a2a185dbae1c7faea9b2b
joobumjin <bumjin.joo@gmail.com>
2024-05-10 06:27:13 -0400

    override default

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import { Manrope, PT_Serif } from "next/font/google";
+import './index.css'
 
 const manrope = Manrope({
   subsets: ['latin'],
